87d19fb9a353b826f6be9c146b9b6b3fc4c21d4f
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2025-02-15 09:05:28 +0530

    fixes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar/>
+      <Form/>
+    </>
+  )
+}
+
+export default App
